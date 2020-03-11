5554bfa11f73e4dbb0d2423fe9a63e3800c33695
Kyle Talmage <ktalmage85@gmail.com>
2020-03-11 18:52:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ class Student
      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
-  def self.create(name)
+  def self.create(name,grade)
     student = Student.new(name,grade)
       student.save
       student
